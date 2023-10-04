d04a96a46257a3cd21dab14e4fab4f05626cbc22
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-10-04 18:27:10 -0300

    solved part 2

--- 01/app/Main.hs
+++ 01/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.List.Split
+import Data.List
 
 main :: IO ()
 main = interact solve
@@ -8,7 +9,7 @@ main = interact solve
 solve :: String -> String
 solve input =
   let elves = map convertElf $ splitInput input
-   in show $ maximum elves
+   in show . sum $ take 3 $ (reverse . sort) elves
   where
     convertElf = sum . map read
     splitInput = splitWhen (== "") . lines
