4b336f4ac67d4fb1226f679d9a92bdd59f5bfcaf
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-10-06 20:25:54 -0300

    Solved part 02 of problem 02

--- 02/app/Main.hs
+++ 02/app/Main.hs
@@ -1,51 +1,61 @@
 module Main where
 
 main :: IO ()
-main = interact solve
+main = interact $ show . solve
 
-solve :: String -> String
-solve xs = show . sum $ roundPoints <$> parseInput xs
+solve :: String -> Int
+solve xs = sum $ roundResult <$> parseInput xs
 
-data Play
+data Shape
     = Rock
     | Paper
     | Scissors
     deriving (Eq, Show)
 
+data Outcome
+    = Win
+    | Loss
+    | Draw
+    deriving (Eq, Show)
+
 data Round =
-    Round Play Play
+    Round Shape Outcome
     deriving (Show)
 
-parsePlay :: Char -> Play
+parsePlay :: Char -> Shape
 parsePlay inp
-    | inp == 'A' || inp == 'X' = Rock
-    | inp == 'B' || inp == 'Y' = Paper
-    | inp == 'C' || inp == 'Z' = Scissors
+    | inp == 'A' = Rock
+    | inp == 'B' = Paper
+    | inp == 'C' = Scissors
+
+parseOutcome :: Char -> Outcome
+parseOutcome inp
+    | inp == 'X' = Loss
+    | inp == 'Y' = Draw
+    | inp == 'Z' = Win
 
 parseRound :: String -> Round
-parseRound (opp:_:me:_) = Round (parsePlay opp) (parsePlay me)
+parseRound (opp:_:me:_) = Round (parsePlay opp) (parseOutcome me)
 
 parseInput :: String -> [Round]
 parseInput inp = map parseRound $ lines inp
 
-shapePoints :: Play -> Int
+shapePoints :: Shape -> Int
 shapePoints Rock = 1
 shapePoints Paper = 2
 shapePoints Scissors = 3
 
 win = 6
+
 draw = 3
+
 loss = 0
 
 roundResult :: Round -> Int
-roundResult (Round Rock Paper) = win
-roundResult (Round Paper Scissors) = win
-roundResult (Round Scissors Rock) = win
-roundResult (Round Rock Scissors) = loss
-roundResult (Round Paper Rock) = loss
-roundResult (Round Scissors Paper) = loss
-roundResult _ = draw
-
-roundPoints :: Round -> Int
-roundPoints r@(Round _ m) = shapePoints m + roundResult r
-
+roundResult (Round a Draw) = draw + shapePoints a
+roundResult (Round Rock Win) = shapePoints Paper + win
+roundResult (Round Rock Loss) = shapePoints Scissors + loss
+roundResult (Round Paper Win) = shapePoints Scissors + win
+roundResult (Round Paper Loss) = shapePoints Rock + loss
+roundResult (Round Scissors Win) = shapePoints Rock + win
+roundResult (Round Scissors Loss) = shapePoints Paper + loss
