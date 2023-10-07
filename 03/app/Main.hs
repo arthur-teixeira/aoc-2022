16f2140e540b00a70d56cbe75a45e1d47525ea3f
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2023-10-07 08:32:38 -0300

    problem 3 part 2

--- 03/app/Main.hs
+++ 03/app/Main.hs
@@ -4,21 +4,26 @@ import Data.List
 import Data.Maybe
 
 main :: IO ()
-main = interact $ (++"\n") . show . solve
+main = interact $ (++ "\n") . show . solve
 
-solve = sum . map itemPriority . lines
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf size xs
+  | length xs > size =
+    case splitAt size xs of
+      (chunk, ys) -> chunk : chunksOf size ys
+  | otherwise = [xs]
+
+solve = sum . map itemPriority . chunksOf 3 . lines
   where
-    itemPriority = getItemPriority . itemInBothCompartments . compartments
+    itemPriority = getItemPriority . getCommonItem . tuplify3
 
-type Rucksack = (String, String)
+tuplify3 :: [a] -> (a, a, a)
+tuplify3 (x:y:z:_) = (x, y, z)
 
-compartments :: String -> Rucksack
-compartments xs = splitAt (length xs `div` 2) xs
-
-itemInBothCompartments :: Rucksack -> Char
-itemInBothCompartments (x:xs, ys)
-  | x `elem` ys = x
-  | otherwise = itemInBothCompartments (xs, ys)
+getCommonItem :: (String, String, String) -> Char
+getCommonItem (x:xs, y, z)
+  | x `elem` y && x `elem` z = x
+  | otherwise = getCommonItem (xs, y, z)
 
 getItemPriority :: Char -> Int
 getItemPriority x = (1 +) $ fromMaybe 0 (x `elemIndex` chars)
