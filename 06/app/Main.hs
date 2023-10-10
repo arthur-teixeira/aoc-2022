baa0c9d45c789503cab49196eb8ce506da463a56
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-10-10 11:45:52 -0300

    solved problem 6 part 1 and 2

---
+++ 06/app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+import Data.List
+
+main :: IO ()
+main = interact $ show . firstUnique . substrs
+
+substrs :: String -> [String]
+substrs xs = filter ((== 14) . length) $ map (take 14) $ tails xs
+
+firstUnique :: [String] -> Int
+firstUnique xs = firstUnique' xs $ (length . head) xs
+
+firstUnique' :: [String] -> Int -> Int
+firstUnique' [] _ = 0
+firstUnique' (x:xs) n 
+    | areUnique x = n
+    | otherwise = firstUnique' xs (n + 1)
+
+areUnique :: String -> Bool
+areUnique [] = True
+areUnique (x:xs)
+    | x `elem` xs = False
+    | otherwise = areUnique xs
