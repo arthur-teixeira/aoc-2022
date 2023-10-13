b0ff5037e302553f78712f52b9ddf0dfffdb09f1
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-10-13 16:42:51 -0300

    actually generating a filesystem tree from the input

--- 07/app/Main.hs
+++ 07/app/Main.hs
@@ -11,70 +11,76 @@ main :: IO ()
 main = putStrLn "Hello, Haskell!"
 
 data Line
-  = CdTop -- $ cd /
-  | CdUp -- $ cd ..
-  | CdDown T.Text -- $ cd <folder> 
-  | Ls -- $ ls
-  | LsDir -- dir <name>
-  | LsFile Int -- <size> <name>
-  deriving (Eq, Show)
+    = CdTop -- $ cd /
+    | CdUp -- $ cd ..
+    | CdDown T.Text -- $ cd <folder> 
+    | Ls -- $ ls
+    | LsDir -- dir <name>
+    | LsFile Int -- <size> <name>
+    deriving (Eq, Show)
 
 parseLine :: T.Text -> Line
 parseLine xs =
-  case T.words xs of
-    ["$", "cd", "/"] -> CdTop
-    ["$", "cd", ".."] -> CdUp
-    ["$", "cd", name] -> CdDown name
-    ["$", "ls"] -> Ls
-    ["dir", _] -> LsDir
-    [size, _] -> LsFile $ toInt size
+    case T.words xs of
+        ["$", "cd", "/"] -> CdTop
+        ["$", "cd", ".."] -> CdUp
+        ["$", "cd", name] -> CdDown name
+        ["$", "ls"] -> Ls
+        ["dir", _] -> LsDir
+        [size, _] -> LsFile $ toInt size
 
 toInt :: T.Text -> Int
 toInt xs =
-  case T.decimal xs of
-    Left _ -> 0
-    Right (x, _) -> x
+    case T.decimal xs of
+        Left _ -> 0
+        Right (x, _) -> x
 
 parse :: String -> [Line]
 parse = map parseLine . T.lines . T.pack
 
+stripLs :: [Line] -> [Line]
+stripLs = filter (/= Ls) . filter (/= LsDir)
+
+makeFileSystem xs = 
+  let input = stripLs . parse $ xs
+      initialTree = Folder "/" []
+      initialState :: State
+      initialState = (initialTree, ["/"]) in
+      foldl (flip doLine) initialState input
+
 data Tree
-  = Folder T.Text [Tree]
-  | File Int
-  deriving (Eq, Show)
+    = Folder T.Text [Tree]
+    | File Int
+    deriving (Eq, Show)
 
-a =
-  Folder "/" [Folder "asdf" [File 123, File 234], Folder "ahbdfg" [], File 235]
-
---createFolder "newFolder" (a, ["/", "asdf"])
-b =
-  Folder
-    "/"
-    [ Folder "asdf" [File 123, File 234, Folder "newFolder" []]
-    , Folder "ahbdfg" []
-    , File 235
-    ]
-
--- Hold the node "/" as the tree and a list of paths to the current folder, like
--- (Tree, [T.Text]) as a state, take a command and execute it on the tree
 type State = (Tree, [T.Text])
 
 newFolder :: T.Text -> Tree
-newFolder name = Folder name []
+newFolder = flip Folder []
 
-insertFolder :: State -> Tree -> Tree
-insertFolder (Folder name files, [curFolder]) newFolder =
-  Folder name (newFolder : files)
-
-insertFolder (Folder curFolderName curFolderFiles, curFolder:nextFolder:path) newFolder =
-  let ([next], folders) = partition isNextFolder curFolderFiles
-      newF = insertFolder (next, nextFolder : path) newFolder
-   in Folder curFolderName (newF : folders)
+treeAppend :: State -> Tree -> Tree
+treeAppend (Folder name files, [curFolder]) newFolderName =
+    Folder name (newFolderName : files) -- TODO: don't insert if already exists
+treeAppend (Folder curFolderName curFolderFiles, _:nextFolder:path) newFolderName =
+    let ([next], folders) = partition isNextFolder curFolderFiles
+        newF = treeAppend (next, nextFolder : path) newFolderName
+     in Folder curFolderName (newF : folders)
   where
     isNextFolder (File _) = False
     isNextFolder (Folder name _) = name == nextFolder
 
+doCdDown :: State -> T.Text -> State
+doCdDown state@(_, oldPath) newFolderName =
+    let newTree = treeAppend state (newFolder newFolderName)
+     in (newTree, oldPath ++ [newFolderName])
+
+doFile :: State -> Int -> State
+doFile state@(_, oldPath) newFileSize =
+    let newTree = treeAppend state (File newFileSize)
+     in (newTree, oldPath)
+
 doLine :: Line -> State -> State
 doLine CdTop (tree, _) = (tree, ["/"])
 doLine CdUp (tree, path) = (tree, init path)
-doLine (CdDown folder) (tree, path) = (tree, path ++ [folder])
+doLine (CdDown folder) (tree, path) = doCdDown (tree, path) folder
+doLine (LsFile fileSize) (tree, path) = doFile (tree, path) fileSize
