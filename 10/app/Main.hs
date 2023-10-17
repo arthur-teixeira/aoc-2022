24a2b4c179d48c2be8e398c1da459c4cd77b7c43
arthur-carvalho-teixeira <acarvalho.objective@rededor.com.br>
2023-10-17 11:38:34 -0300

    Solved problem 10 part 02

--- 10/app/Main.hs
+++ 10/app/Main.hs
@@ -2,19 +2,20 @@ module Main where
 
 main :: IO ()
 main = do
-  contents <- getContents
-  print $ solve contents
+    contents <- getContents
+    putStrLn $ partOne contents
+    putStrLn $ partTwo contents
 
 data Instruction
-  = Noop
-  | Addx Int
-  deriving (Show)
+    = Noop
+    | Addx Int
+    deriving (Show)
 
 parseInstruction :: String -> Instruction
 parseInstruction xs =
-  case words xs of
-    ["addx", x] -> Addx $ read x
-    ["noop"] -> Noop
+    case words xs of
+        ["addx", x] -> Addx $ read x
+        ["noop"] -> Noop
 
 parse :: String -> [Instruction]
 parse = map parseInstruction . lines
@@ -29,10 +30,38 @@ getCycles = reverse . foldl doInstruction [1]
 getIndexes :: [Int] -> [Int] -> [Int]
 getIndexes idxs xs = [x | (i, x) <- zip [1 ..] xs, i `elem` idxs]
 
-solve :: String -> Int
-solve xs =
-  let cycles = getCycles . parse $ xs
-      cyclesToCheck = [20,60 .. 220] :: [Int]
-      checked = getIndexes cyclesToCheck cycles
-      mult = zipWith (*) cyclesToCheck checked
-   in sum mult
+partOne :: String -> String
+partOne xs =
+    let cycles = getCycles . parse $ xs
+        cyclesToCheck = [20,60 .. 220] :: [Int]
+        checked = getIndexes cyclesToCheck cycles
+        mult = zipWith (*) cyclesToCheck checked
+     in "Part one: " ++ (show . sum) mult
+
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+chunks n xs = 
+  let (chunk, rest) = splitAt n xs
+    in chunk : chunks n rest
+
+partTwo :: String -> String
+partTwo xs =
+    let cycles = init . getCycles . parse $ xs
+        pixels = drawPixels cycles
+     in "Part two: \n" ++ (unlines . chunks 40) pixels
+
+spriteRange :: Int -> [Int]
+spriteRange x = [x - 1, x, x + 1]
+
+drawPixels = drawPixels' 0
+
+drawPixels' :: Int -> [Int] -> [Char]
+drawPixels' _ [] = []
+drawPixels' curCycleIndex (curX:xs) =
+    let curPixel = drawPixel curX
+     in curPixel : drawPixels' (curCycleIndex + 1) xs
+  where
+    drawPixel x
+        | (curCycleIndex `mod` 40) `elem` spriteRange x = '#'
+        | otherwise = '.'
+
